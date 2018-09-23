c03827f69c3ac638b4216e13f391254611c8f874
wangchengchen <1425375190@qq.com>
2018-09-23 18:58:12 +0800

    第一次提交

---
+++ src/com/itheima/test/GitTest.java
@@ -0,0 +1,10 @@
+package com.itheima.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("I love java");
+        System.out.println("I love java");
+        System.out.println("I love java");
+        System.out.println("I love java");
+    }
+}
